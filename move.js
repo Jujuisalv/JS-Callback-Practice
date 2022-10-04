e15ad09222366c85b5850fc5d14b935bd9aa78f6
Julius Alvarez <Juliusisaiaha@gmail.com>
2022-10-04 15:19:19 -0700

    great
    great

--- move.js
+++ move.js
@@ -6,7 +6,54 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowkeys(left, bottom, gifCode) {      
+        let direction = null;
+        let x = 100;
+        let y = 250;
+
+        function moveCharacter() {
+            if (direction === 'north') {
+                y = y + 1
+            } else if (direction === 'east') {
+                x = x + 1
+            } else if (direction === 'south') {
+                y = y - 1
+            } else if (direction === 'west') {
+                x = x - 1
+            } else {
+                x = x
+                y = y
+            }
+
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+
+        setInterval(moveCharacter, 1)
+
+        document.addEventListener('keydown', function(e) {
+            if (e.repeat) return;
+
+            if (e.key === 'ArrowUp') {
+                direction = 'north'
+            } else if (e.key === 'ArrowRight') {
+                direction = 'east'
+            } else if (e.key === 'ArrowDown') {
+                direction = 'south'
+            } else if (e.key === 'ArrowLeft') {
+                direction = 'west'
+            }
+            gifCode(direction)
+        })
+
+        document.addEventListener('keyup', function(e) {
+            direction = null
+            gifCode(direction)
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowkeys
     }
 }
